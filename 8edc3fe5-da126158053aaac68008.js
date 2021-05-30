67388a94b8c2ea75ea68454bb5c017a753b3df9d
Elvis <gilbertbatista.k@gmail.com>
2021-05-30 16:09:26 -0400

    Updates

---
+++ 8edc3fe5-da126158053aaac68008.js
@@ -0,0 +1,2 @@
+(self.webpackChunkmy_portafolio=self.webpackChunkmy_portafolio||[]).push([[821],{4863:function(e,r,t){"use strict";t.d(r,{cRw:function(){return i}});var n,o=t(1121),c=t(7294),i=c.forwardRef((function(e,r){var t=e.children,i=(0,o._)(e,["children"]);return c.createElement(o.I,(0,o.a)({width:16,height:16,viewBox:"0 0 16 16",xmlns:"http://www.w3.org/2000/svg",fill:"currentColor",ref:r},i),n||(n=c.createElement("path",{d:"M8 11L3 6 3.7 5.3 8 9.6 12.3 5.3 13 6z"})),t)}))}}]);
+//# sourceMappingURL=8edc3fe5-da126158053aaac68008.js.map
