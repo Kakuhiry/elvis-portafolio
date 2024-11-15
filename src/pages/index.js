d9ea996e00d627c76a6fcd0246d3248ee1abb81c
Gilbert Batista <gilbert_batista@billet.do>
2024-11-15 01:54:56 -0500

    fix: removed react-pdf

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import AboutMe from './AboutMe';
 import Jobs from '../components/Jobs';
 import Education from './Education';
 // Import Document and Page from react-pdf
-import { Document, Page } from 'react-pdf';
+
 
 function App() {
   const [selectedPage, setSelectedPage] = useState(1);
